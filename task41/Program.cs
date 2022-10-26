790490685eee3179cd3182f6da2650116c87e78f
Nastya <anastasic.curly@mail.ru>
2022-10-26 18:44:13 +0300

    Add task41

---
+++ task41/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear ();
+Console.Write ("Введите кол-во чисел: ");
+int n=Convert.ToInt32 (Console.ReadLine());
+int b=0;
+string number=string.Empty;
+string posit = string.Empty;
+int count =0;
+int i=0;
+while (i !=n)
+{
+    Console.Write ("Введите число: ");
+    b= Convert.ToInt32 (Console.ReadLine());
+    if (b>0)
+    {
+posit= posit+Convert.ToString (b)+ " ";
+        count=count+1;
+    }
+    number=number+Convert.ToString (b)+ " ";
+    i++;
+    }
+    Console.Write ("Кол-во положительных чисел: ");
+    Console.WriteLine (count);
+     Console.Write ("Положительные числа: ");
+     Console.WriteLine (posit);
